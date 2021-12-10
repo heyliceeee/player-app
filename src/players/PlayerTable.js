82a6738ef564f99cd41d625d8ff00ffec5f89c72
heyliceeee <alicedias2002@hotmail.com>
2021-12-10 18:22:53 +0000

    update players playersTable

--- src/players/PlayerTable.js
+++ src/players/PlayerTable.js
@@ -1,30 +1,19 @@
 import './PlayerTable.css';
+import React from 'react';
+import { Table } from 'antd';
 
 
 const PlayerTable = (props) => {
 
-    const renderHobbies = (hobbies) => {
-        return hobbies.map((hobbie) => {
-            return (
-                <label key={ hobbie._id }>{ hobbie.name };</label>
-            )
-        })
-    }
-
     return (
-        <li className="player">
-            <div className="player-cell player-name">
-                <label className="player-label">Name: { props.name }</label>
-            </div>
-
-            <div className="player-cell player-lastName">
-                <label className="player-label">LastName: { props.lastName }</label>
-            </div>
-
-            <div className="player-cell player-hobbies">
-                <label className="player-label">Hobbies: { renderHobbies(props.hobbies) }</label>
-            </div>
-        </li>
+        <Table>
+            columns = { columns }
+            rowKey = { record => record._id }
+            dataSource = { players }
+            pagination = { pagination }
+            loading = { loading }
+            onChange = { handleTableChange }
+        </Table>
     )
 }
 
--- src/players/Players.js
+++ src/players/Players.js
@@ -1,16 +1,14 @@
-import Player from './PlayerTable';
 import './Players.css';
 import Config from '../config';
 import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router';
 import PlayersForm from './add/PlayersForm';
 import { Link } from 'react-router-dom';
+import PlayerTable from './PlayerTable';
 
 
 const Players = () => {
 
-    const [loading, setLoading] = useState(true);
-    const [players, setPlayers] = useState([]);
     const [showForm, setShowForm] = useState(false);
 
     const onClickShowForm = () => {
@@ -20,32 +18,9 @@ const Players = () => {
     //esta msg irá aparecer dentro do btn
     const showFormMessage = showForm ? 'Hide Form' : 'Show Form';
 
-    useEffect(() => {
-        fetch('/team/players', {
-            headers: { 'Accept': 'application/json', 'x-access-token': Config.token }
-        })
-
-        .then((response) => response.json())
-
-        .then((response) => {
-            const { auth, players = [] } = response;
-
-            if(auth){
-                setLoading(false);
-                setPlayers(players);
-            }
-        });
-
-        return () => setPlayers([]);
-    }, [])
-
 
     if(!Config.token){
         return <Navigate to={'/'}></Navigate>
-    }
-
-    if(loading){
-        return <h1>LOADING</h1>
     }
 
 
@@ -57,7 +32,9 @@ const Players = () => {
             </div>
 
             <label>PLAYERS: </label>
-            <div className='player-container'>{ players.map((player) => <Player key={ player._id } { ...player }></Player>) }</div>
+            <div className='player-container'>
+                <PlayerTable></PlayerTable>
+            </div>
 
 
             { showForm && <PlayersForm></PlayersForm> }
